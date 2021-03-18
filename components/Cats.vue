6ac2bb3d2bea12720187c11ded11c4fb579f0946
Oliver Leitner <never@neverslair-blog.net>
2021-03-18 21:18:46 +0100

    lil bit of playing around with rxjs

--- components/Cats.vue
+++ components/Cats.vue
@@ -15,19 +15,57 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
+import { AxiosResponse } from "axios";
+// import Rx from 'rxjs/Rx';
+// import VueRx from 'vue-rx';
+import { Subject, Subscription } from "rxjs";
 
-@Component({})
+const subject = new Subject();
+
+export const messageService = {
+  sendMessage: (message: any) => subject.next({ text: message }),
+  clearMessages: () => subject.next(),
+  getMessage: () => subject.asObservable(),
+};
+
+@Component<Cats>({})
 export default class Cats extends Vue {
-  public fetchedData = null;
+  public fetchedData = [];
+  public subscription: Subscription = new Subscription();
 
-  public async fetchData() {
-    let aData = await this.$axios.get("https://cat-fact.herokuapp.com/facts");
+  public async fetchData(): Promise<any> {
+    let aData = <AxiosResponse>(
+      await this.$axios.get("https://cat-fact.herokuapp.com/facts")
+    );
     let outData = await aData.data;
     this.fetchedData = outData;
+    return outData;
   }
 
-  mounted() {
+  /*async mounted() {
+    this.$subscribeTo(await this.fetchData(), function (val) {
+      console.log(val)
+    })
     this.fetchData();
+  }*/
+
+  created() {
+    // subscribe to home component messages
+    /*this.subscription = messageService.getMessage().subscribe((fetchData) => {
+      if (fetchData) {
+        // add message to local state if not empty
+        this.fetchedData.push(<never>fetchData);
+      } else {
+        // clear messages when empty message received
+        this.fetchedData = [];
+      }
+    });*/
+    this.fetchData()
   }
+
+  /*beforeDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.subscription.unsubscribe();
+  }*/
 }
 </script>
